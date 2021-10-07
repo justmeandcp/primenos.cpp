b6fe05dbf974a158bc0f5788c1ab68952f595f20
justmeandcp <91773245+justmeandcp@users.noreply.github.com>
2021-10-07 07:34:21 +0530

    Create factors.cpp
    Complexity O(n)

---
+++ factors.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {    
+
+int n;
+cin>>n;
+int i;
+int c=0;
+cout<<"All factors are"<<endl;
+
+for(i=1;i<n;i++)
+{
+if(n%i==0)
+cout<<i<<endl;
+}
+
+
+
+
+
+
+
+   
+    return 0;
+}
